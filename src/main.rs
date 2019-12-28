54964bb0904bd56223765a9fb17bc8c7f2564f18
Zane Hitchcox <zwhitchcox@gmail.com>
2019-12-27 21:22:09 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+    }
+}
+fn main() {
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+}
