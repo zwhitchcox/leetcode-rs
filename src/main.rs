544fcdb9008297f624f8965c8c7acc47dad6df9d
Zane Hitchcox <zwhitchcox@gmail.com>
2020-01-01 17:34:19 -0500

    3

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,42 @@
 pub struct Solution;
-// `print_refs` takes two references to `i32` which have differentpub struct Solution;
-
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
-
-pub fn to_list(vec: Vec<i32>) -> List {
-    let mut current = None;
-    for &v in vec.iter().rev() {
-        let mut node = ListNode::new(v);
-        node.next = current;
-        current = Some(Box::new(node));
-    }
-    current
-}
-
-type List = Option<Box<ListNode>>;
+use std::collections::HashMap;
 
 impl Solution {
-    pub fn add_two_numbers(l1: List, l2: List) -> List {
-        let mut l1 = &l1;
-        let mut l2 = &l2;
-        let mut carry = 0;
-        let mut result = None;
-        let mut cur = &mut result;
-
-        while l1.is_some() || l2.is_some() || carry != 0 {
-            let mut sum = carry;
-            if let Some(node) = l1 {
-                sum += node.val;
-                l1 = &node.next;
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let (mut longest, mut start) = (0, 0);
+        let mut hash: HashMap<char, usize> = HashMap::new();
+        for (i, c) in s.chars().enumerate() {
+            if let Some(&j) = hash.get(&c)  {
+                if j >= start {
+                    longest = longest.max(i - start);
+                    start = j + 1;
+                }
             }
-            if let Some(node) = l2 {
-                sum += node.val;
-                l2 = &node.next;
-            }
-            carry = sum / 10;
-            *cur = Some(Box::new(ListNode::new(sum % 10)));
-            cur = &mut cur.as_mut().unwrap().next;
+            hash.insert(c, i);
         }
-
-        result
+        longest.max(s.len() - start) as i32
     }
-}
+ }
 
 fn main() {
     assert_eq!(
-        Solution::add_two_numbers(to_list(vec![2,4,3]), to_list(vec![5, 6, 4])),
-        to_list(vec![7, 0, 8])
+        Solution::length_of_longest_substring(String::from("bba")),
+        2
+    );
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("abcabcbb")),
+        3
+    );
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("abba")),
+        2
+    );
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("cdd")),
+        2
+    );
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from(" ")),
+        1
     );
 }
