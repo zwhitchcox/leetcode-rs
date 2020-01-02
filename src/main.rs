3e2a11ba2a68fc9862034feb22ffdcc846413222
Zane Hitchcox <zwhitchcox@gmail.com>
2020-01-01 22:06:13 -0500

    4

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,17 @@ use std::i32::MAX;
 
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        let (mut i, mut j) = (0, 0);
-        let total_length = nums1.len() + nums2.len();
-        let median = total_length / 2;
-        let mut cur = 0;
-        let mut last = 0;
-        while i + j <= median {
-            let num1 = nums1.get(i).or(Some(&MAX)).unwrap();
-            let num2 = nums2.get(j).or(Some(&MAX)).unwrap();
-            if num1 < num2 {
-                i += 1;
-                last = cur;
-                cur = *num1;
-            } else {
-                j += 1;
-                last = cur;
-                cur = *num2;
-            }
-        }
+        let mut nums = nums1;
+        nums.extend(nums2);
+        nums.sort();
+        let median = nums.len() / 2;
+
         
-        if total_length % 2 == 0 {
-            return (last + cur) as f64 / 2.0;
+        if nums.len() % 2 == 0 {
+            return (nums[median - 1] + nums[median]) as f64 / 2.0;
         }
 
-        cur as f64
+        nums[median] as f64
     }
 }
 
